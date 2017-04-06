c4b41bacf2ff338047e6cac781871b52e752a875
MaxMEllon <maxmellon1994@gmail.com>
2017-04-06 20:59:43 +0900

    Update

--- bin/runnner.rb
+++ bin/runnner.rb
@@ -1,8 +1,7 @@
-
 input_data = ''
 (0..100).each { input_data << Random.rand(3).to_s << "\n" }
 
-File.write('hoge.txt', input_data)
+File.write('input.txt', input_data)
 
 pid = spawn('java Game -classpath ./src', in: 'input.txt', out: '/dev/stdout', err: '/dev/stderr')
 Process.detach(pid)
